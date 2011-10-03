cafdc707628059ff4c84aa5de5faad041008dc12
Łukasz "Cyber Killer" Korpalski <cyber_killer@jabster.pl>
2011-10-03 18:16:22 +0200

    simple help, argument handling, regexp strings put in variables
    --HG--
    extra : convert_revision : cyber_killer%40jabster.pl-20111003161622-lw6eeeit5p7mrlao

--- CkOpenMetalCastCueSheetGenScript.php
+++ CkOpenMetalCastCueSheetGenScript.php
@@ -20,7 +20,28 @@
 	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+if($_SERVER["argc"] != 1)
+{
 
+$timeRegExp="^(\d+:\d+)\s.+\sby\s.+\sfrom\s.+\s\(.+\)$";
+$nameRegExp="^\d+:\d+\s(.+)\sby\s.+\sfrom\s.+\s\(.+\)$";
+$artistRegExp="^\d+:\d+\s.+\sby\s(.+)\sfrom\s.+\s\(.+\)$";
+//$albumRegExp="^\d+:\d+\s.+\sby\s.+\sfrom\s(.+)\s\(.+\)$";
+//$licenseRegExp="^\d+:\d+\s.+\sby\s.+\sfrom\s.+\s\((.+)\)$";
+
+
+//preg_match('/[\d]+/', $_SERVER["argv"][$num], $albumid);
+
+
+}
+else
+{
+	print("CkOpenMetalCastCueSheetGenScript - a script for generating cue sheet files for Open MetalCast");
+	print("Version 0.1 by Cyber Killer\n");
+	print("Get the latest version at http://digital.dharkness.info/CkOpenMetalCastCueSheetGenScript\n\n");
+	print("Usage:\n");
+	print($_SERVER["argv"][0] . " <txt file with tracklist copied from Open MetalCast website>\n");
+}
 
 ?>
 
